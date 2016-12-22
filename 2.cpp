6809b6419eac08f49779a945072a4236d2a913f9
ciec20151104714 <944174132@qq.com>
2016-12-22 19:27:07 +0800

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -46,3 +46,39 @@ void loop() {
   // 让LED神灯再次点亮。
   // 如此不断循环下去。
 }
+
+
+#include <stdio.h>
+#define N 5;
+struct Node
+{
+    char name[40];
+    int age;
+    struct Node*next;
+};
+int main()
+{
+    struct Node *head,*p,*tail;
+    int i;
+    head=new Node;
+    head->next=0;
+    tail=head;
+    scanf("%s",head->name);
+    scanf("%d",&head->age);
+    for(i=0;i<5;i++)
+    {
+        p=new Node;
+        scanf("%s",p->name);
+        scanf("%d",&p->age);
+        p->next=0;
+        tail->next=p;
+        tail=p;
+    }
+    p=head;
+    while(p)
+    {
+        printf("%s %d \n",p->name,p->age);
+        p=p->next;
+    }
+    return 0;
+}
